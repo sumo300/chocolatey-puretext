b8815ead765363be5aad53bbf809bcb23a7231ef
Sumerano, Michael J <mjsumerano@geisinger.edu>
2016-12-13 07:42:31 -0500

    Added checksums

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -1,4 +1,17 @@
-Install-ChocolateyZipPackage -PackageName 'PureText' -Url 'http://stevemiller.net/downloads/puretext_4.0_32-bit.zip' -Url64 'http://stevemiller.net/downloads/puretext_4.0_64-bit.zip' -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = 'PureText'
+  url            = 'http://stevemiller.net/downloads/puretext_4.0_32-bit.zip'
+  url64          = 'http://stevemiller.net/downloads/puretext_4.0_64-bit.zip'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  checksum       = '57A2C8DBD70CB1F8E72DFBA35A64E699A4B8728F91A2702BBDF0C1CC5F4C4F79'
+  checksumType   = 'sha256'
+  checksum64     = '2AC4685C20CB15CADD54787F9DEFD13F7505F7BE9CFC2F5A1CD0774AE4EEB8D2'
+  checksum64Type = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
 
 $hr = "="*[System.Console]::WindowWidth
 Write-Host $hr
